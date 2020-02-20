d22abf8e680cbe38382b51d5819cbfab63784ae3
Daming <daming@umich.edu>
2020-02-20 15:30:00 -0500

    fix names

--- src/component/Bar.js
+++ src/component/Bar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class SubTest extends Component {
+export default class Bar extends Component {
   render() {
     const ratio = 70;
     return (
--- src/component/Bars.js
+++ src/component/Bars.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Bar from './Bar';
 
-export default class Test1 extends Component {
+export default class Bars extends Component {
   constructor(props) {
     super(props);
     this.state = {
